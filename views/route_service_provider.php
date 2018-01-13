1638f862325300075d80f62fcba523fa63daca45
abdelqader <abdelqader@eaa.com.sa>
2018-01-14 00:34:26 +0200

    fixes

--- views/route_service_provider.php
+++ views/route_service_provider.php
@@ -2,7 +2,6 @@
 
 namespace App\Providers;
 
-//use App\Http\Controllers\AccIndependentReviewerController;
 use Route;
 use Illuminate\Routing\Router;
 use Illuminate\Foundation\Support\Providers\RouteServiceProvider as ServiceProvider;
